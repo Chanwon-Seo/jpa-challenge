65952b8c3afa598c2b87396ae878490ecf4758e0
Chanwon-Seo <scwonn60@gmail.com>
2024-06-17 18:16:52 +0900

    init

---
+++ src/test/java/com/scw/jpachallenge/JpaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scw.jpachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
